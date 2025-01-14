e40c053b7e1f518d31f88c4e44718f16b87a16a6
ppwlsw <pinpawat.l@ku.th>
2025-01-14 18:51:05 +0700

    updated:course_result seeder/ RestAPI

---
+++ database/seeders/CourseResultSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Course;
+use App\Models\Student;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CourseResultSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $courses = Course::all();
+        $students = Student::all();
+
+        foreach ($students as $student) {
+            // Attach each student to random courses
+            $studentCourses = $courses->random(rand(1, 3)); // Each student enrolls in 1-3 random courses
+
+            foreach ($studentCourses as $course) {
+                DB::table('course_result')->insert([
+                    'course_id' => $course->id,
+                    'student_id' => $student->id,
+                    'semester' => ['1', '2'][array_rand(['1', '2'])], // Random semester
+                    'academic_year' => rand(2020, 2025), // Random academic year
+                    'course_grade' => ['A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'F'][array_rand(['A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'F'])], // Random grade
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
+    }
+}
